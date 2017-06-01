51c74ecf7aee03071eb14b4bc1ad99b75d07bea4
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-06-01 12:38:48 +0200

    Code refactoring

---
+++ groupes.py
@@ -0,0 +1,32 @@
+import csv
+import numpy
+
+from utils import get_contraintes, equilibrage
+
+__author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
+
+# Etape 1 : Récupérer les horaires des différents groupes dans le fichier data/horaires.csv
+horaires = {}
+for row in csv.reader(open("data/horaires.csv", "r"), delimiter=";"):
+    horaires[row[0]] = row[1]
+
+# Etape 2 : Récupérer les choix d'option des étudiants dans le fichier data/input_data.csv
+etudiants = {}
+for row in csv.reader(open("data/input_data.csv", "r"), delimiter=";"):
+    etudiants[row[0]] = row[1:]
+
+numpy.random.seed(0)  # Uniquement pour pouvoir faire tourner plusieurs fois et obtenir les mêmes groupes
+
+# Etape 3 : créer des groupes (potentiellement déséquilibrés)
+etudiants_dans_les_groupes = {}
+for numetu, list_options in etudiants.items():
+    contraintes = get_contraintes(horaires, list_options)
+    etudiants_dans_les_groupes[numetu] = list(contraintes.values())
+
+# Etape 4 : équilibrer les groupes
+etudiants_dans_les_groupes = equilibrage(etudiants_dans_les_groupes, horaires)
+
+# Etape 5 : Ecrire les groupes dans un fichier
+fp = open("data/groupes_equilibres.csv", "w")
+for numetu, list_groupes in etudiants.items():
+    fp.write(numetu + ";" + ";".join(list_groupes) + "\n")
---
+++ utils.py
@@ -0,0 +1,84 @@
+import numpy
+
+__author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
+
+
+def get_list_groupes(option, contraintes, horaires, list_options):
+    if option not in list_options:
+        return ["NO_%s" % option]
+    groupes_existants = [k for k in horaires.keys() if k.startswith(option)]
+    numpy.random.shuffle(groupes_existants)
+    groupes_possibles = []
+    for gr in groupes_existants:
+        if horaires[gr] not in contraintes.keys():
+            groupes_possibles.append(gr)
+    return groupes_possibles
+
+
+def get_contraintes(horaires, list_options):
+    contraintes = {}
+    for gr_bdd in get_list_groupes("BDD", contraintes, horaires, list_options):
+        if gr_bdd != "NO_BDD":
+            contraintes[horaires[gr_bdd]] = gr_bdd
+        for gr_tel in get_list_groupes("TEL", contraintes, horaires, list_options):
+            if gr_tel != "NO_TEL":
+                contraintes[horaires[gr_tel]] = gr_tel
+            for gr_ent in get_list_groupes("ENT", contraintes, horaires, list_options):
+                if gr_ent != "NO_ENT":
+                    contraintes[horaires[gr_ent]] = gr_ent
+                return contraintes
+            del contraintes[horaires[gr_tel]]
+        del contraintes[horaires[gr_bdd]]
+    return None
+
+
+
+def compte(etudiants, option):
+    listes = {}
+    for numetu, list_groups in etudiants.items():
+        for gr in list_groups:
+            if gr.startswith(option):
+                if gr not in listes.keys():
+                    listes[gr] = []
+                listes[gr].append(numetu)
+    return listes
+
+
+def dict_argmin(d):
+    minval, argmin = numpy.inf, None
+    for k, v in d.items():
+        if v < minval:
+            argmin = k
+            minval = v
+    return argmin, minval
+
+
+def dict_argmax(d):
+    maxval, argmax = -numpy.inf, None
+    for k, v in d.items():
+        if v > maxval:
+            argmax = k
+            maxval = v
+    return argmax, maxval
+
+
+def equilibrage(etudiants, horaires):
+    for option in ["BDD", "TEL", "ENT"]:
+        has_moved = True
+        while has_moved:
+            listes = compte(etudiants, option)
+            tailles = {k: len(v) for k, v in listes.items()}
+            argmin_taille, min_taille = dict_argmin(tailles)
+            argmax_taille, max_taille = dict_argmax(tailles)
+            if max_taille - min_taille < 2:
+                break
+            has_moved = False
+            for numetu, list_groupes in etudiants.items():
+                if argmax_taille in list_groupes:
+                    list_horaires = [horaires[gr] for gr in list_groupes if gr != argmax_taille]
+                    if horaires[argmin_taille] not in list_horaires:
+                        etudiants[numetu] = [gr for gr in list_groupes if gr != argmax_taille] + [argmin_taille]
+                        has_moved = True
+                        break
+        print("Après équilibrage : %s" % str({k: len(v) for k, v in compte(etudiants, option).items()}))
+    return etudiants
